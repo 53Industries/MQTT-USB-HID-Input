05e7a917fe4a986609c57a3fe052c446f81c5830
53Industries <74923945+53Industries@users.noreply.github.com>
2022-06-02 16:01:14 -0600

    Update mqtt-keyboard.py

--- mqtt-keyboard.py
+++ mqtt-keyboard.py
@@ -2,32 +2,37 @@ import paho.mqtt.client as mqtt
 import serial
 import time
 
+# Update to match com port and baud rate
 arduino = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=.1)
-# The callback for when the client receives a CONNACK response from the server.
+
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe("camdisplay/office/keyboard")
+    # Topic to listen to on for actions
+    client.subscribe("camdisplay/livingroom/keyboard")
 
-# The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    print("MQTT: "+msg.topic+" "+str(msg.payload))
     send = msg.payload.decode("utf-8")
     arduino.write(msg.payload)
-    #write_read(msg.payload)
-    print(send)
+
+def write_read(x):
+    arduino.write(x)
+    time.sleep(0.05)
+    data = arduino.readline()
+    print(data)
+    print("Arduino End")
     
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-# Update to match MQTT Server setup
-client.connect("127.0.0.1", 1883, 60)
+# Update to match server settings
+client.connect("192.168.30.11", 1883, 60)
 
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
-client.loop_forever()
+while True:
+    serialData = arduino.readline()[:-2]
+    if serialData:
+        print("Arduino: "+str(serialData))
+    client.loop_start()
+
